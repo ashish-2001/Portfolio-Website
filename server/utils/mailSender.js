6d1f1f4c076cf15e6c470796d8e57c8dcfbdf703
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-09-20 20:11:49 +0530

    Fixed some bugs

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,23 +1,30 @@
 import nodemailer from "nodemailer";
 
-async function sendMail(to, subject, text){
+async function sendMail(name, email, message){
 
     try{
 
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: process.env.MAIL_HOST,
+            port: 587,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
-            }
+            },
+            secure: false
         });
 
         
     const info = await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        text
+        from: `Portfolio Contact <${process.env.EMAIL_USER}>`,
+        to: process.env.MAIL_RECEIVER,
+        subject: `New message from ${name}`,
+        text: `
+                <h3>Contact Details</h3>
+                <p><strong>Name:</strong> ${name}</p>
+                <p><strong>Email:</strong> ${email}</p>
+                <p><strong>Message:</strong> ${message}</p>
+            `
     })
 
     console.log("Email sent", info.response);
